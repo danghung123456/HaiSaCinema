71d113cbada8e7ac3a8c16733fc4addda45ead53
hunghenry1995 <hunghenry1995@gmail.com>
2020-10-03 16:27:58 +0700

    push len branch dev nha

--- src/main/java/com/haisa/CinemaApplication.java
+++ src/main/java/com/haisa/CinemaApplication.java
@@ -8,7 +8,7 @@ public class CinemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
-		System.out.println("123");
+		System.out.println("push code len branch dev nha mng");
 	}
 
 }

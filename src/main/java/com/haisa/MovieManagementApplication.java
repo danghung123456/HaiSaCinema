51239a7d283dd4c21ed49d3acb9c3d1e73bf5e77
Thai Vo <quangthai2120@gmail.com>
2020-10-06 15:56:04 +0700

    Cho tai ne

---
+++ src/main/java/com/haisa/MovieManagementApplication.java
@@ -0,0 +1,14 @@
+package com.haisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("123");
+		System.out.println("Đăng");
+		System.out.println("Thái said: Tui là Sói nè");
+	}
+}

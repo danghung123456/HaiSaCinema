51c177baa4018f1cce94b074a200ceadd49b242b
ldt15420 <ldt15420@gmail.com>
2020-10-06 15:50:53 +0700

    11

--- src/main/java/com/haisa/CinemaApplication.java
+++ src/main/java/com/haisa/CinemaApplication.java
@@ -9,15 +9,7 @@ public class CinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 
-		System.out.println("Hưng test");
-		
 
-
-		System.out.println("push code len branch dev nha mng");
-
-		System.out.println("Thai ne");
-System.out.println("Tai ne");
-System.out.println(" Quang Thai ne");
 	}
 
 
